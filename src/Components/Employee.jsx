355cbb24ddfe1160ffe12c3248439caaf3c29366
Sowparna Shome <soupornoshome@gmail.com>
2024-04-28 23:06:50 +0530

    dependancy changes

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,33 +1,108 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+// import axios from 'axios'
+// import { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+
+// function Employee() {
+//   const [data, setData] = useState([])
+
+//   useEffect(()=> {
+//     axios.get('https://localhost:7036/api/Employee')
+//     .then(res => {
+//       if(res.data.Status === "Success") {
+//         setData(res.data);
+//         console.log(data)
+//       } else {
+//         alert("Error")
+//       }
+//     })
+//     .catch(err => console.log(err));
+//   }, [])
+
+//   const handleDelete = (id) => {
+//     axios.delete('https://localhost:7036/api/delete/'+id)
+//     .then(res => {
+//       if(res.data.Status === "Success") {
+//         window.location.reload(true);
+//       } else {
+//         alert("Error")
+//       }
+//     })
+//     .catch(err => console.log(err));
+//   }
+
+//   return (
+//     <div className='px-5 py-3'>
+//       <div className='d-flex justify-content-center mt-2'>
+//         <h3>Employee List</h3>
+//       </div>
+//       <Link to="/create" className='btn btn-success'>Add Employee</Link>
+//       <div className='mt-3'>
+//         <table className='table'>
+//           <thead>
+//             <tr>
+//               <th>Name</th>
+//               <th>Image</th>
+//               <th>Email</th>
+//               <th>Address</th>
+//               <th>Salary</th>
+//               <th>Action</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {data.map((employee) => {
+//               return <tr key={employee.employeeId}>
+//                   <td>{employee.employeeName}</td>
+//                   <td>{
+//                     <img src={`http://localhost:8081/images/`+employee.image} alt="" className='employee_image'/>
+//                     }</td>
+//                   <td>{employee.email}</td>
+//                   <td>{employee.address}</td>
+//                   <td>{employee.salary}</td>
+//                   <td>
+//                     <Link to={`/employeeEdit/`+employee.id} className='btn btn-primary btn-sm me-2'>edit</Link>
+//                     <button onClick={()=> handleDelete(employee.id)} className='btn btn-sm btn-danger'>delete</button>
+//                   </td>
+//               </tr>
+//             })}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Employee
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Employee() {
-  const [data, setData] = useState([])
+  const [datas, setData] = useState([]);
 
-  useEffect(()=> {
-    axios.get('api/getEmployee')
-    .then(res => {
-      if(res.data.Status === "Success") {
-        setData(res.data.Result);
-      } else {
-        alert("Error")
-      }
-    })
-    .catch(err => console.log(err));
-  }, [])
+  useEffect(() => {
+    axios.get('https://localhost:7036/api/Employee')
+      .then(res => {
+        if (res.data.Status === "Success") {
+          setData(res.data);
+          console.log(datas); // Log the response data directly
+        } else {
+          alert("Error");
+        }
+      })
+      .catch(err => console.log(err));
+  }, [datas]);
 
   const handleDelete = (id) => {
-    axios.delete('https://localhost:7036/api/delete/'+id)
-    .then(res => {
-      if(res.data.Status === "Success") {
-        window.location.reload(true);
-      } else {
-        alert("Error")
-      }
-    })
-    .catch(err => console.log(err));
-  }
+    axios.delete('https://localhost:7036/api/delete/' + id)
+      .then(res => {
+        if (res.data.Status === "Success") {
+          window.location.reload(true);
+        } else {
+          alert("Error");
+        }
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <div className='px-5 py-3'>
@@ -40,7 +115,7 @@ function Employee() {
           <thead>
             <tr>
               <th>Name</th>
-              <th>Image</th>
+              {/* <th>Image</th> */}
               <th>Email</th>
               <th>Address</th>
               <th>Salary</th>
@@ -48,26 +123,28 @@ function Employee() {
             </tr>
           </thead>
           <tbody>
-            {data.map((employee, index) => {
-              return <tr key={index}>
-                  <td>{employee.name}</td>
-                  <td>{
-                    <img src={`http://localhost:8081/images/`+employee.image} alt="" className='employee_image'/>
-                    }</td>
+            {datas.map((employee) => {
+              return (
+                <tr key={employee.employeeId}>
+                  <td>{employee.employeeName}</td>
+                  {/* <td>
+                    <img src={`http://localhost:8081/images/` + employee.image} alt="" className='employee_image' />
+                  </td> */}
                   <td>{employee.email}</td>
                   <td>{employee.address}</td>
                   <td>{employee.salary}</td>
                   <td>
-                    <Link to={`/employeeEdit/`+employee.id} className='btn btn-primary btn-sm me-2'>edit</Link>
-                    <button onClick={()=> handleDelete(employee.id)} className='btn btn-sm btn-danger'>delete</button>
+                    <Link to={`/employeeEdit/` + employee.id} className='btn btn-primary btn-sm me-2'>edit</Link>
+                    <button onClick={() => handleDelete(employee.id)} className='btn btn-sm btn-danger'>delete</button>
                   </td>
-              </tr>
+                </tr>
+              );
             })}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default Employee
+export default Employee;
